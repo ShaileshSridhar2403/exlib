d079f4b4a824d1f5359c08d58201affc6001f0c5
ShaileshSridhar2403 <shailesh.sridhar@gmail.com>
2024-03-18 18:54:09 -0400

    modified lime api to support additional retain_counts argument 5

--- src/exlib/explainers/lime.py
+++ src/exlib/explainers/lime.py
@@ -3,8 +3,9 @@ import torch.nn.functional as F
 import lime
 import math
 import numpy as np
-from lime import lime_image
+from lime import lime_image, lime_text, lime_base
 from .common import *
+from lime.lime_text import LimeTextExplainer
 
 
 def lime_cls_closure(model, collapse):
@@ -225,93 +226,3 @@ class LimeImageSeg(FeatureAttrMethod):
 #                 model_regressor=model_regressor,
 #                 feature_selection=self.feature_selection)
 #         return ret_exp
-    
-
-def lime_cls_closure_text(model, tokenizer):
-    def go(x_str):
-        x_raw = [tokenizer.decode(tokenizer.convert_tokens_to_ids(x_str_i.split())) for x_str_i in x_str]
-        inp = tokenizer(x_raw, 
-                        padding='max_length', 
-                        truncation=True, 
-                        max_length=512)
-        inp = {k: torch.tensor(v).to(next(model.parameters()).device) 
-               for k, v in inp.items()}
-        y = model(**inp)
-        return y.detach().cpu().numpy()
-    return go
-
-def explain_text_cls_with_lime(model, tokenizer, x, ts,
-                                LimeTextExplainerKwargs={},
-                                # Gets FA for every label if top_labels == None
-                                explain_instance_kwargs={}):
-    """
-    Explain a pytorch model with LIME.
-    this function is not intended to be called directly.
-    We only explain one text at a time.
-
-    # LimeImageExplainer args
-    kernel_width=0.25, kernel=None, verbose=False, feature_selection='auto', random_state=None
-
-    # explain_instance args
-    image, classifier_fn, labels=(1, ), hide_color=None, top_labels=5,
-    num_features=100000, num_samples=1000, batch_size=10, segmentation_fn=None,
-    distance_metric='cosine', model_regressor=None, random_seed=None, progress_bar=true
-
-    # get_image_and_mask arguments
-    positive_only=true, negative_only=False, hide_rest=False, num_features=5, min_weight=0.0
-    """
-
-    ## Texts here are not batched
-    # L = x.shape
-    tokens = tokenizer.convert_ids_to_tokens(x)
-    x_str = ' '.join(tokens)
-
-    f = lime_cls_closure_text(model, tokenizer)
-    explainer = LimeTextExplainer(**LimeTextExplainerKwargs)
-
-    if isinstance(ts, torch.Tensor):
-        todo_labels = ts.numpy()
-    else:
-        todo_labels = ts
-
-    lime_exp = explainer.explain_instance(x_str, f, labels=todo_labels, **explain_instance_kwargs)
-    
-    explanation_dict = {w: s for w, s in lime_exp.as_list()}
-    
-    attrs = torch.tensor([explanation_dict[token] if token in explanation_dict else 0 for token in tokens])
-
-    return FeatureAttrOutput(attrs, lime_exp)
-
-
-class LimeTextCls(FeatureAttrMethod):
-    def __init__(self, model, tokenizer,
-                 LimeTextExplainerKwargs={},
-                 explain_instance_kwargs={
-                     # Make this however big you need to get every label
-                     # this is because the original LIME API is stupid
-                     "top_labels" : 1000000,
-                     "num_samples" : 500,
-                 }):
-        super().__init__(model)
-        self.tokenizer = tokenizer
-        self.LimeTextExplainerKwargs = LimeTextExplainerKwargs
-        self.explain_instance_kwargs = explain_instance_kwargs
-
-    def forward(self, x, t):
-        if not isinstance(t, torch.Tensor):
-            t = torch.tensor(t)
-
-        N = x.size(0)
-        assert x.ndim == 2 and t.ndim == 1 and len(t) == N
-
-        attrs, lime_exps = [], []
-        for i in range(N):
-            xi, ti = x[i], t[i].cpu().item()
-            out = explain_text_cls_with_lime(self.model, self.tokenizer, xi, [ti],
-                    LimeTextExplainerKwargs=self.LimeTextExplainerKwargs,
-                    explain_instance_kwargs=self.explain_instance_kwargs)
-
-            attrs.append(out.attributions)
-            lime_exps.append(out.explainer_output)
-
-        return FeatureAttrOutput(torch.stack(attrs), lime_exps)
